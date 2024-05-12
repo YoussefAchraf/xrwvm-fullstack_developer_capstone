4b18e142a82949697cfd8dec934870664f3b0dba
YoussefAchraf <achrafyoussef.ay@gmail.com>
2024-05-12 15:24:10 -0400

    new commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path(route='logout', view=views.logout_user, name='logout'),
 
     # path for add a review view
-
+        path(route='get_cars', view=views.get_cars, name ='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
